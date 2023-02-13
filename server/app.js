09321d7cfed9c25de3ace08f14626dbc0feb7871
Martin <martin.torrado.mouzo@gmail.com>
2023-02-13 13:00:03 +0000

    Add middleware and token model with stubs

--- server/app.js
+++ server/app.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const cors = require("cors");
+const logRoutes = require("./middleware/logger");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(logRoutes);
 
 app.get("/", (req, res) => {
     res.json({
