7a84da1b47e41a968e1e7c18e92312503e3117d2
Martin <martin.torrado.mouzo@gmail.com>
2023-02-13 12:52:51 +0000

    add token table to setup.sql

--- server/database/setup.sql
+++ server/database/setup.sql
@@ -1,4 +1,5 @@
 DROP TABLE IF EXISTS user_account;
+DROP TABLE IF EXISTS token;
 
 CREATE TABLE user_account (
     user_id INT GENERATED ALWAYS AS IDENTITY,
@@ -6,4 +7,12 @@ CREATE TABLE user_account (
     password VARCHAR(100) NOT NULL,
     email VARCHAR(100) NOT NULL,
     PRIMARY KEY (user_id)
+);
+
+CREATE TABLE token (
+    token_id INT GENERATED ALWAYS AS IDENTITY,
+    user_id INT NOT NULL,
+    token CHAR(200) UNIQUE NOT NULL,
+    PRIMARY KEY(token_id),
+    FOREIGN KEY(user_id) REFERENCES user_account("user_id")
 );
