84a8e5738a0171689acd120df1a5c4592cf85086
Mark Halstead <103516032+Mark-Halstead@users.noreply.github.com>
2023-02-14 10:38:39 +0000

    Added new initiatives section

--- static/welcome.js
+++ static/welcome.js
@@ -1,3 +1,4 @@
+// Scrolls to div with the id of content on click
 function scrollToElement() {
     const element = document.getElementById("content");
     element.scrollIntoView({ behavior: "smooth" });
