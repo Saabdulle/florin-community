e90af31a811a92c493b75fe02b949534c962827c
Mark Halstead <103516032+Mark-Halstead@users.noreply.github.com>
2023-02-15 16:48:37 +0000

    Added some addional passed backend tests

---
+++ tests/post.test.js
@@ -0,0 +1,37 @@
+const Post = require("../server/models/post")
+let db = require("../server/database/connect")
+
+describe("Testing the getAll function", () => {
+    it('getAll returns an array', async () => {
+        const posts = await Post.getAll();
+        expect(posts).toBeInstanceOf(Array);
+    });
+})
+
+
+describe('Post', () => {
+
+  describe('create', () => {
+    it('should create a new post', async () => {
+      const data = { title: 'Test Post', post_body: 'This is a test post.' };
+      const user_id = 1;
+
+      const post = await Post.create(data, user_id);
+
+      expect(post instanceof Post).toBe(true);
+
+      const result = await db.query('SELECT * FROM post WHERE post_id = $1', [post.post_id]);
+      expect(result.rows.length).toBe(1);
+      expect(result.rows[0].title).toBe(data.title);
+      expect(result.rows[0].post_body).toBe(data.post_body);
+      expect(result.rows[0].user_id).toBe(user_id);
+    });
+  });
+});
+
+  
+  
+
+
+
+
