28f7d06daa684d6b8707057aacdcf1e9b59ddf4c
Cameron <amoatengcameron@hotmail.com>
2023-02-16 09:31:46 +0000

    add delete function to post

--- server/routers/post.js
+++ server/routers/post.js
@@ -5,6 +5,6 @@ const postRouter = Router();
 
 postRouter.get("/", postController.index);
 postRouter.post("/", postController.create);
-//postRouter.patch("/", postController.update);
-
+postRouter.patch("/", postController.update);
+postRouter.delete("/:id", postController.destroy)
 module.exports = postRouter;
