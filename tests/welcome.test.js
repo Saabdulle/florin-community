7f006b62418bab815bce3b6699aea20719e57be3
Mark Halstead <103516032+Mark-Halstead@users.noreply.github.com>
2023-02-16 11:34:59 +0000

    Passed some frontend tests

--- tests/welcome.test.js
+++ tests/welcome.test.js
@@ -2,7 +2,7 @@ const { scrollToElement, scrollToInitiatives, scrollToVolunteer } = require("./w
 global.fetch = require("jest-fetch-mock")
 
 const fs = require("fs");
-window.document.body.innerHTML = fs.readFileSync("../client/HTML/welcome.html")
+window.document.body.innerHTML = fs.readFileSync("./client/HTML/welcome.html")
 
 
 describe("Testing scrollToElement", () => {
@@ -10,6 +10,30 @@ describe("Testing scrollToElement", () => {
 
     it("gets the element using the DOM", () => {
         const element = document.getElementById("content");
+        scrollToElement()
+        expect(element).not.toBe(null)
+    })
+
+})
+
+describe("Testing scrollToInitiatives", () => {
+
+
+    it("gets the element using the DOM", () => {
+        const element = document.getElementById("initiatives");
+        scrollToInitiatives()
+        expect(element).not.toBe(null)
+    })
+
+})
+
+
+describe("Testing scrollToVolunteer", () => {
+
+
+    it("gets the element using the DOM", () => {
+        const element = document.getElementById("volunteer");
+        scrollToVolunteer()
         expect(element).not.toBe(null)
     })
 
