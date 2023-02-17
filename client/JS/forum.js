01fdcee290527949c0e3927096ca808b66028249
Saeed Abdulle <104086456+Saabdulle@users.noreply.github.com>
2023-02-17 12:14:27 +0000

    Dark mode icon in html pages

--- client/JS/forum.js
+++ client/JS/forum.js
@@ -67,3 +67,7 @@ getThreads()
 function donationPage() {
     window.location="../HTML/donation.html"
 }
+function changeTheme() {
+    let element = document.body;
+    element.classList.toggle("dark-mode");
+ }
