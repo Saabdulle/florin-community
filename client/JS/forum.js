7d87c6c8501bb49d8c0f28f8446a377d94825276
Martin <martin.torrado.mouzo@gmail.com>
2023-02-17 01:06:01 +0000

    check signin and signup and inform user of error

--- client/JS/forum.js
+++ client/JS/forum.js
@@ -65,5 +65,5 @@ document.getElementById("create-thread").addEventListener("submit", async (e)=>{
 getThreads()
 
 function donationPage() {
-    window.location="http://127.0.0.1:5500/client/HTML/donation.html"
+    window.location="../HTML/donation.html"
 }
