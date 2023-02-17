a604c8138f30e7d06d4570e3a40ce61e9bb77d9e
Saeed Abdulle <104086456+Saabdulle@users.noreply.github.com>
2023-02-17 11:22:46 +0000

    Signin page switch colour theme

--- client/JS/signin.js
+++ client/JS/signin.js
@@ -30,3 +30,7 @@ document.getElementById("signin-form").addEventListener("submit", async (e) => {
 function donationPage() {
     window.location="../HTML/donation.html"
 }
+function changeTheme() {
+    let element = document.body;
+    element.classList.toggle("dark-mode");
+ }
